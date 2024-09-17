5e8588ed15267e01c06da400bb54e37f8bd1cbce
GauriiiR <Gauri.Rewatkar@ibm.com>
2024-09-17 10:32:32 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4.java
@@ -0,0 +1,16 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4 {
+	@Test
+	public void Test1() {
+		System.out.println("Activity_4_2 Test 1");
+	}
+
+	@Test
+	public void Test2() {
+		System.out.println("Activity_4_2 Test 2");
+	}
+
+}
---
+++ TestNG/Activities/Activity4__2.java
@@ -0,0 +1,16 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4__2 {
+
+	@Test
+	public void Test1() {
+		System.out.println("Activity4 Test 1");
+	}
+
+	@Test
+	public void Test2() {
+		System.out.println("Activity4 Test 2");
+	}
+}
